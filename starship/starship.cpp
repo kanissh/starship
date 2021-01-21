6ce93bc8af9bfa2e7b50217bb3e7a10e3229964c
kanissh <kanissh11@gmail.com>
2021-01-21 21:23:03 +0530

    refactor

--- starship/starship.cpp
+++ starship/starship.cpp
@@ -1,65 +1,273 @@
-#include<windows.h>
-#include<GL/glut.h>
+#include <GL/glut.h>  
+#include <math.h>
 
-int refreshMillis = 30;
+// vertices for the cube
+GLfloat x = 2.0f;
+GLfloat y = 2.0f;
+GLfloat z = 2.0f;
 
-void  init() {
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH); 
-	glEnable(GL_DEPTH_TEST);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(0.0,0.0,0.0,1.0);
+// variables to move outermost Object Frame (to move all the rendered environment)
+GLfloat moveX = 0.0f;
+GLfloat moveY = 0.0f;
+GLfloat moveZ = 0.0f;
+
+// variables to rotate outermost Object Frame (to move all the rendered environment)
+GLfloat rotX = 0.0f;
+GLfloat rotY = 0.0f;
+GLfloat rotZ = 0.0f;
+
+
+// For animating the rotation of the objects
+GLfloat animateRotation = 0.0f;
+
+//variables to move the camera
+GLfloat camY = 0.0f;
+GLfloat camX = 0.0f;
+GLfloat camZ = 0.0f;
+
+
+void init() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+
+}
+
+void drawAxes() {
+
+    glBegin(GL_LINES);
+
+    glLineWidth(1.5);
+
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(-20, 0, 0);
+    glVertex3f(20, 0, 0);
+
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(0, -20, 0);
+    glVertex3f(0, 20, 0);
+
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(0, 0, -20);
+    glVertex3f(0, 0, 20);
+
+    glEnd();
+}
+void DrawGrid() {
+    GLfloat ext = 20.0f;
+    GLfloat step = 1.0f;
+    GLfloat yGrid = -0.4f;
+    GLint line;
+
+    glBegin(GL_LINES);
+    for (line = -ext; line <= ext; line += step) {
+        glVertex3f(line, yGrid, ext);
+        glVertex3f(line, yGrid, -ext);
+
+        glVertex3f(ext, yGrid, line);
+        glVertex3f(-ext, yGrid, line);
+    }
+    glEnd();
+}
+
+void drawCube() {
+    // glTranslatef(0.0, 1.6, 0.0);
+            // BACK
+    glBegin(GL_QUADS);
+    glColor3f(0.4f, 0.3f, 0.5f);
+    glVertex3f(x, y, z);
+    glVertex3f(x, -y, z);
+    glVertex3f(-x, -y, z);
+    glVertex3f(-x, y, z);
+    glEnd();
+
+    // FRONT
+    glBegin(GL_QUADS);
+    glColor3f(0.1, 0.5, 0.3);
+    glVertex3f(x, y, -z);
+    glVertex3f(-x, y, -z);
+    glVertex3f(-x, -y, -z);
+    glVertex3f(x, -y, -z);
+    glEnd();
+
+    // LEFT
+    glBegin(GL_QUADS);
+    glColor3f(0.3, 0.5, 0.6);
+    glVertex3f(-x, -y, -z);
+    glVertex3f(-x, y, -z);
+    glVertex3f(-x, y, z);
+    glVertex3f(-x, -y, z);
+    glEnd();
+
+    //Right
+    glBegin(GL_QUADS);
+    glColor3f(0.2, 0.2, 0.2);
+    glVertex3f(x, y, z);
+    glVertex3f(x, -y, z);
+    glVertex3f(x, -y, -z);
+    glVertex3f(x, y, -z);
+    glEnd();
+
+    //Top
+    glBegin(GL_QUADS);
+    glColor3f(0.6, 0.0, 0.0);
+    glVertex3f(x, y, -z);
+    glVertex3f(x, y, z);
+    glVertex3f(-x, y, z);
+    glVertex3f(-x, y, -z);
+    glEnd();
+
+    //Bottom
+    glBegin(GL_QUADS);
+    glColor3f(0.4, 0.0, 0.4);
+    glVertex3f(x, -y, z);
+    glVertex3f(x, -y, -z);
+    glVertex3f(-x, -y, -z);
+    glVertex3f(-x, -y, z);
+    glEnd();
 }
 
 void display() {
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glEnable(GL_DEPTH_TEST);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-	glBegin(GL_QUADS);              
-	glColor3f(1.0f, 0.0f, 0.0f); 
-	glVertex2f(-0.5f, -0.5f);    
-	glVertex2f(0.5f, -0.5f);
-	glVertex2f(0.5f, 0.5f);
-	glVertex2f(-0.5f, 0.5f);
-	glEnd();
 
-	glFlush();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	
+
+    glPushMatrix();
+
+    // camera orientation (eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ)
+    gluLookAt(0.0 + camX, 2.0 + camY, 5.0 + camZ, 0, 0, 0, 0, 1.0, 0);
+
+    // move the object frame using keyboard keys
+    glTranslatef(moveX, moveY, moveZ);
+    glRotatef(rotX, 1.0f, 0.0f, 0.0f);
+    glRotatef(rotY, 0.0f, 1.0f, 0.0f);
+    glRotatef(rotZ, 0.0f, 0.0f, 1.0f);
+
+
+    glColor3f(1.0, 1.0, 1.0);
+
+    DrawGrid();
+
+    //Cube
+    glPushMatrix();
+    glRotatef(animateRotation, 0.0, 1.0, 0.0);
+    glTranslatef(0.0, 1.6, 0.0);
+
+    drawCube();
+
+    glPopMatrix();
+
+    //Torus(Doughnut)
+    glPushMatrix();
+    glColor3f(0.5, 0.5, 0.5);
+    glTranslatef(10.0, 3.0, 10.0);
+    glRotatef(-animateRotation, 0.0, 1.0, 0.0);
+    glutSolidTorus(1, 2, 50, 50);
+
+    glPopMatrix();
+
+    // Cone
+    glPushMatrix();
+    glColor3f(0.5, 0.7, 0.3);
+    glTranslatef(-10.0, 3.0, 10.0);
+    glRotatef(-animateRotation, 1.0, 0.0, 0.0);
+    glutSolidCone(1, 3, 80, 100);
+
+    glColor3f(0.7, 0.3, 0.5);
+    glRotatef(180.0, 1.0, 0.0, 0.0);
+    glutSolidCone(1, 3, 80, 100);
+
+    glPopMatrix();
+
+    //Teapot
+    glPushMatrix();
+    glColor3f(0.9, 0.1, 0.5);
+    glTranslatef(10.0, 1.0, -10.0);
+    glRotatef(-animateRotation, 0.0, 1.0, 0.0);
+
+    glTranslatef(0.0, 0.0, -1.0);
+    glutSolidTeapot(1);
+
+
+    glPopMatrix();
+
+    drawAxes();
+    glPopMatrix();
+
+    glutSwapBuffers();
+
 }
 
-void reshape(GLsizei width, GLsizei height) {
-	if (height == 0) height = 1;
+void keyboardSpecial(int key, int x, int y) {
+    if (key == GLUT_KEY_UP)
+        moveZ += 1;
 
-	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+    if (key == GLUT_KEY_DOWN)
+        moveZ -= 1;
 
-	glViewport(0,0,width,height);
-	glMatrixMode(GL_PROJECTION);
+    if (key == GLUT_KEY_LEFT)
+        rotY -= 5.0;
 
-	glLoadIdentity();
+    if (key == GLUT_KEY_RIGHT)
+        rotY += 1.0;
 
-	gluPerspective(45.0, aspect, 0.1, 100.0);
-
-	if (width <= height) {
-		glOrtho(-1.0, 1.0, -1.0 / aspect, 1.0 / aspect, -1.0, 1.0);  
-	}
-	else {
-		glOrtho(-1.0 * aspect, 1.0 * aspect, -1.0, 1.0, -1.0, 1.0);  
-	}
+    glutPostRedisplay();
 }
 
-void timer(int value) {
-	glutPostRedisplay();
-	glutTimerFunc(refreshMillis, timer, 0);
+void keyboard(unsigned char key, int x, int y) {
+    if (key == 'w')
+        camY += 0.5;
+    if (key == 's')
+        camY -= 0.5;
+
+    if (key == 'c')
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    if (key == 'C')
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    glutPostRedisplay();
+
+}
+
+void Timer(int x) {
+    animateRotation += animateRotation >= 360.0 ? -animateRotation : 5;
+    glutPostRedisplay();
+
+    glutTimerFunc(60, Timer, 1);
+}
+
+void changeSize(GLsizei w, GLsizei h) {
+    glViewport(0, 0, w, h);
+    GLfloat aspect_ratio = h == 0 ? w / 1 : (GLfloat)w / (GLfloat)h;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluPerspective(120.0, aspect_ratio, 1.0, 100.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+
 }
 
 int main(int argc, char** argv) {
-	glutInit(&argc, argv);
-	glutCreateWindow("Starship");
-	glutInitWindowSize(500,500);
-	glutInitWindowPosition(50,50);
-	glutDisplayFunc(display);
-	init();
-	glutMainLoop();
-	return 0;
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(150, 150);
+    glutCreateWindow("OpenGL Setup Test");
+    glutDisplayFunc(display);
+    glutReshapeFunc(changeSize);
+
+    // keyboard function activation
+    glutKeyboardFunc(keyboard);
+    glutSpecialFunc(keyboardSpecial);
+
+    glutTimerFunc(60.0, Timer, 1);
+    init();
+    glutMainLoop();
+
+
+    return 0;
 }
