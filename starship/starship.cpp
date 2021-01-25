4d464f9e84d304b2c43329640231cdcb09826345
kanissh <kanissh11@gmail.com>
2021-01-25 16:44:03 +0530

    Added rocket cylinders and top cone

--- starship/starship.cpp
+++ starship/starship.cpp
@@ -1,302 +1,288 @@
-#include<stdlib.h>
+#include <cmath>
 #include <iostream>
 #include <GL/glut.h>  
-#include <math.h>
 
+#define PI 3.1415927
 
-using namespace std;
-
-// vertices for the cube
-GLfloat x = 2.0f;
-GLfloat y = 2.0f;
-GLfloat z = 2.0f;
-
-// variables to move outermost Object Frame (to move all the rendered environment)
 GLfloat moveX = 0.0f;
 GLfloat moveY = 0.0f;
 GLfloat moveZ = 0.0f;
 
-// variables to rotate outermost Object Frame (to move all the rendered environment)
 GLfloat rotX = 0.0f;
 GLfloat rotY = 0.0f;
 GLfloat rotZ = 0.0f;
 
-
-// For animating the rotation of the objects
-GLfloat animateRotation = 0.0f;
-
-//variables to move the camera
 GLfloat camY = 0.0f;
 GLfloat camX = 0.0f;
 GLfloat camZ = 0.0f;
 
-// A quadratic object pointer used to draw the sides of the cylinder
+bool showWireframe = true;
+bool showAxes = true;
+bool showGrid = true;
+
+
+GLfloat starshipHeight = 4.2;
+GLfloat superheavyHeight = 7;
 GLUquadricObj* qobj;
 
-//Starship dim
-GLfloat starshipHeight = 10.0;
-
-
-//Super heavy dim
-GLfloat superHeavyHeight = 14.0;
+GLfloat topConeHeight = 0.8;
+GLfloat topConeTopRad = 0.2;
+GLfloat topConeBottomRad = 1;
 
 void init() {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glEnable(GL_DEPTH_TEST);
-
-    // Inititalization of the quadratic object for the cylinder (one time inititalization is only needed ).
-    // This can be used to draw any number of cylinders
-    qobj = gluNewQuadric();
-    gluQuadricDrawStyle(qobj, GLU_FILL);
-    gluQuadricNormals(qobj, GLU_SMOOTH);
-
-    // glPolygonMode(GL_FRONT, GL_LINE);
-    // glPolygonMode(GL_BACK, GL_FILL);
-    // glEnable(GL_CULL_FACE);
-
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	qobj = gluNewQuadric();
+	gluQuadricDrawStyle(qobj, GLU_FILL);
+	gluQuadricNormals(qobj, GLU_SMOOTH);
 }
-
-void DrawGrid() {
-    GLfloat ext = 20.0f;
-    GLfloat step = 1.0f;
-    GLfloat yGrid = -0.4f;
-    GLint line;
-
-    glBegin(GL_LINES);
-    for (line = -ext; line <= ext; line += step) {
-        glVertex3f(line, yGrid, ext);
-        glVertex3f(line, yGrid, -ext);
-
-        glVertex3f(ext, yGrid, line);
-        glVertex3f(-ext, yGrid, line);
-    }
-    glEnd();
-}
-
-
-void drawCylinder(GLdouble base, GLdouble top, GLdouble height, GLdouble slices, GLdouble stacks) {
-    glPushMatrix();
-    // Triangle Fan for the Base
-    /*glBegin(GL_TRIANGLE_FAN);
-    glVertex3f(0.0, 0.0, 0.0);
-    for (float i = 0; i < 360; i += 1.2) {
-        glVertex3f(base * cos(i), base * sin(i), 0.0);
-        glVertex3f(base * cos(i + 0.2), base * sin(i + 0.2), 0.0);
-    }
-    glEnd();*/
-
-    // Sides of the Cylinder
-    gluCylinder(qobj, base, top, height, slices, stacks);
-
-    // Triangle Fan for the Top
-    glTranslatef(0.0, 0.0, height);
-
-   /* glBegin(GL_TRIANGLE_FAN);
-    glVertex3f(0.0, 0.0, 0.0);
-    for (float i = 0; i < 360; i += 0.2) {
-        glVertex3f(top * cos(i), top * sin(i), 0.0);
-        glVertex3f(top * cos(i + 0.2), top * sin(i + 0.2), 0.0);
-    }
-    glEnd();*/
-    glPopMatrix();
-}
-
 
 void drawAxes() {
+	glBegin(GL_LINES);
 
-    glBegin(GL_LINES);
-    glVertex3f(-2, 0, 0);
-    glVertex3f(2, 0, 0);
+	glLineWidth(1.5);
 
-    glVertex3f(0, -2, 0);
-    glVertex3f(0, 2, 0);
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(-25.0, 0.0, 0.0);
+	glVertex3f(25.0, 0.0, 0.0);
 
-    glVertex3f(0, 0, -2);
-    glVertex3f(0, 0, 2);
+	glColor3f(0.0, 1.0, 0.0);
+	glVertex3f(0.0, -25.0, 0.0);
+	glVertex3f(0.0, 25.0, 0.0);
 
-    glEnd();
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(0.0, 0.0, -25);
+	glVertex3f(0.0, 0.0, 25);
+
+	glEnd();
+}
+
+void drawGrid() {
+	GLfloat ext = 20.0f;
+	GLfloat step = 1.0f;
+	GLfloat yGrid = 0.0f;
+	GLint line;
+
+	glColor3f(1.0, 1.0, 1.0);
+
+	glBegin(GL_LINES);
+	for (line = -ext; line <= ext; line += step) {
+		glVertex3f(line, yGrid, ext);
+		glVertex3f(line, yGrid, -ext);
+
+		glVertex3f(ext, yGrid, line);
+		glVertex3f(-ext, yGrid, line);
+	}
+	glEnd();
+}
+
+void drawCylinder(GLfloat radius, GLfloat height, GLfloat r, GLfloat g, GLfloat b) {
+	GLfloat y = 0.0;
+	GLfloat z = 0.0;
+	GLfloat angle = 0.0;
+	GLfloat angle_stepsize = 0.1;
+
+	glColor3f(r,g,b);
+	// TUBE
+	glBegin(GL_QUAD_STRIP);
+	angle = 2 * PI;
+	while (angle >= 0.0) {
+		y = radius * sin(angle);
+		z = radius * cos(angle);
+		glVertex3f(height, y, z);
+		glVertex3f(0.0, y, z);
+		angle = angle - angle_stepsize;
+	}
+	glVertex3f(height, 0.0, radius);
+	glVertex3f(0.0, 0.0, radius);
+	glEnd();
+
+	// BACK
+	glColor3f(1.0, 1.0, 0.5);
+	glBegin(GL_POLYGON);
+	angle = 0.0;
+	while (angle < 2 * PI) {
+		y = radius * sin(angle);
+		z = radius * cos(angle);
+		glVertex3f(0.0, y, z);
+		angle = angle + angle_stepsize;
+	}
+	glVertex3f(0.0, 0.0, radius);
+	glEnd();
+
+	// FRONT
+	glColor3f(1.0, 1.0, 0.5);
+	glBegin(GL_POLYGON);
+	angle = 2 * PI - angle_stepsize;
+	while (angle >= 0.0) {
+		y = radius * sin(angle);
+		z = radius * cos(angle);
+		glVertex3f(height, y, z);
+		angle = angle - angle_stepsize;
+	}
+	glVertex3f(height, 0.0, radius);
+	glEnd();
+}
+
+void drawStarship() {
+	glPushMatrix();
+	glRotatef(90,0,0,1);
+	glTranslatef(superheavyHeight,0,0);
+	drawCylinder(1,starshipHeight, 1,0,1);
+	glPopMatrix();
+}
+
+
+
+void drawSuperheavy() {
+	glPushMatrix();
+	glRotatef(90, 0, 0, 1);
+	drawCylinder(1, superheavyHeight,1,1,1);
+	glPopMatrix();
+}
+ 
+
+
+
+void drawTopCone() {
+	/*glPushMatrix();
+	glColor3f(1,1,1);
+	glTranslatef(0,starshipHeight+superheavyHeight,0);
+	glRotatef(-90, 1, 0, 0);
+	gluCylinder(qobj, topConeBottomRad, topConeTopRad, topConeHeight, 50, 50);
+	glPopMatrix();*/
+	GLdouble eqn[4] = {1,0,0,0};
+
+	glPushMatrix();
+	glEnable(GL_CLIP_PLANE0);
+	//glRotatef(90, 0, 0, 1);
+	glClipPlane(GL_CLIP_PLANE0, eqn);
+	
+	glutSolidSphere(10, 100, 100);
+	glDisable(GL_CLIP_PLANE0);
+	glPopMatrix();
+	
 }
 
 
 void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	gluLookAt(10.0 + camX, 10.0 + camY, 10.0 + camZ, 0, 0, 0, 0, 1.0, 0);
 
+	glTranslatef(moveX, moveY, moveZ);
+	glRotatef(rotX, 1.0f, 0.0f, 0.0f);
+	glRotatef(rotY, 0.0f, 1.0f, 0.0f);
+	glRotatef(rotZ, 0.0f, 0.0f, 1.0f);
 
+	if (showAxes) {
+		drawAxes();
+	}
 
-    glPushMatrix();
+	if (showGrid) {
+		drawGrid();
+	}
 
-    // camera orientation (eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ)
-    gluLookAt(0.0 + camX, 2.0 + camY, 5.0 + camZ, 0, 0, 0, 0, 1.0, 0);
-
-    // move the object frame using keyboard keys
-    glTranslatef(moveX, moveY, moveZ);
-    glRotatef(rotX, 1.0f, 0.0f, 0.0f);
-    glRotatef(rotY, 0.0f, 1.0f, 0.0f);
-    glRotatef(rotZ, 0.0f, 0.0f, 1.0f);
-
-
-    glColor3f(1.0, 1.0, 1.0);
-
-    //DrawGrid();
-
-    //super heavy
-    glPushMatrix();
-    glColor3f(1.0, 1.0, 1.0);
-    
-    glRotatef(90, 1.0, 0.0, 0.0);
-    
-    drawCylinder(1, 1, superHeavyHeight, 100, 100);
-    glPopMatrix();
-
-    //starship
-    glPushMatrix();
-    glColor3f(1.0, 0.0, 0.0);
-    glTranslatef(0.0,starshipHeight,0.0);
-
-    glRotatef(90, 1.0, 0.0, 0.0);
-
-    drawCylinder(1, 1, starshipHeight, 100, 100);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0, 10, 0);
-    glBegin(GL_POLYGON);
-    glColor3f(1,1,1);
-   
-    glVertex3f(-2,0,0);
-    glVertex3f(2,0,0);
-    glVertex3f(0.86,0.1,0);
-    glVertex3f(0.86, 0.3, 0);
-    glVertex3f(0,1.6,0);
-    glVertex3f(-0.86, 0.3, 0);
-    glVertex3f(-0.86, 0.1, 0);
-    glVertex3f(-2, 0, 0);
-    
-   
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(1, 1, 1);
-    
-    glVertex3f(-2, 0, 0.05);
-    glVertex3f(2, 0, 0.05);
-    glVertex3f(0.86, 0.1, 0.05);
-    glVertex3f(0.86, 0.3, 0.05);
-    glVertex3f(0, 1.6, 0.05);
-    glVertex3f(-0.86, 0.3, 0.05);
-    glVertex3f(-0.86, 0.1, 0.05);
-    glVertex3f(-2, 0, 0.05);
-
-
-    glEnd();
-    glPopMatrix();
-
-
-    glPopMatrix();
-
-    glutSwapBuffers();
-
-}
-
-
-
-void Timer(int x) {
-    animateRotation += animateRotation >= 360.0 ? -animateRotation : 5;
-    glutPostRedisplay();
-
-    glutTimerFunc(60, Timer, 1);
+	//drawStarship();
+	//drawSuperheavy();
+	drawTopCone();
+	glPopMatrix();
+	glutSwapBuffers();
 }
 
 void keyboardSpecial(int key, int x, int y) {
-    if (key == GLUT_KEY_UP) {
-        moveZ += 1;
-    }
-    else if (key == GLUT_KEY_DOWN) {
-        moveZ -= 1;
-    }
-    else if (key == GLUT_KEY_LEFT) {
-        moveX += 1;
-    }
-    else if (key == GLUT_KEY_RIGHT) {
-        moveX -= 1;
-    }
+	if (key == GLUT_KEY_UP) {
+		moveZ += 1;
+	}
+	else if (key == GLUT_KEY_DOWN) {
+		moveZ -= 1;
+	}
+	else if (key == GLUT_KEY_LEFT) {
+		moveX += 1;
+	}
+	else if (key == GLUT_KEY_RIGHT) {
+		moveX -= 1;
+	}
 
-    glutPostRedisplay();
+	glutPostRedisplay();
 }
 
 void keyboard(unsigned char key, int x, int y) {
-    if (key == 'w') {
-        camY += 0.5;
-    }
-    else if (key == 's') {
-        camY -= 0.5;
-    }
-    else if (key == 'a') {
-        rotY += 5.0;
-    }
-    else if (key == 'd') {
-        rotY -= 5.0;
-    }
-    else if (key == '2') {
-        moveY += 1;
-    }
-    else if (key == '8') {
-        moveY -= 1;
-    }
-    /*else if (key == 'c') {
-        if (showWireframe)
-            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        else
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        showWireframe = !showWireframe;
-    }
-    else if (key == 'x') {
-        showAxes = !showAxes;
-    }
-    else if (key == 'g') {
-        showGrid = !showGrid;
-    }*/
-    else if (key == 'r') {
-        camY = -10;
-        rotY = 145;
-        moveY = 0;
-        moveX = 7;
-        moveZ = 10;
-    }
-    else if (key == 'p') {
-        std::cout << camY << "|" << rotY << "|" << moveY << "|" << moveX << "|" << moveZ;
-    }
-    glutPostRedisplay();
+	if (key == 'w') {
+		camY += 0.5;
+	}
+	else if (key == 's') {
+		camY -= 0.5;
+	}
+	else if (key == 'a') {
+		rotY += 5.0;
+	}
+	else if (key == 'd') {
+		rotY -= 5.0;
+	}
+	else if (key == '2') {
+		moveY += 1;
+	}
+	else if (key == '8') {
+		moveY -= 1;
+	}
+	else if (key == 'c') {
+		if (showWireframe)
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		else
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		showWireframe = !showWireframe;
+	}
+	else if (key == 'x') {
+		showAxes = !showAxes;
+	}
+	else if (key == 'g') {
+		showGrid = !showGrid;
+	}
+	else if (key == 'r') {
+		camY = -10;
+		rotY = 145;
+		moveY = 0;
+		moveX = 7;
+		moveZ = 10;
+	}
+	else if (key == 'p') {
+		std::cout << camY << "|" << rotY << "|" << moveY << "|" << moveX << "|" << moveZ;
+	}
+	glutPostRedisplay();
 }
 
 void changeSize(GLsizei w, GLsizei h) {
-    glViewport(0, 0, w, h);
-    GLfloat aspect_ratio = h == 0 ? w / 1 : (GLfloat)w / (GLfloat)h;
+	glViewport(0, 0, w, h);
+	GLfloat aspect_ratio = h == 0 ? w / 1 : (GLfloat)w / (GLfloat)h;
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(120.0, aspect_ratio, 1.0, 100.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(120.0, aspect_ratio, 1.0, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 int main(int argc, char** argv) {
+	glutInit(&argc, argv);
 
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA);
-    glutCreateWindow("OpenGL Setup Test");
-    glutInitWindowSize(700, 700);
-    glutInitWindowPosition(150, 150);
-    glutDisplayFunc(display);
-    glutReshapeFunc(changeSize);
-    // keyboard function activation
-    glutKeyboardFunc(keyboard);
-    glutSpecialFunc(keyboardSpecial);
-    init();
-    glutTimerFunc(60.0, Timer, 1);
-    glutMainLoop();
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA);
 
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(150, 150);
 
-    return 0;
+	glutCreateWindow("Starship");
+
+	glutDisplayFunc(display);
+	glutReshapeFunc(changeSize);
+
+	glutKeyboardFunc(keyboard);
+	glutSpecialFunc(keyboardSpecial);
+
+	init();
+	glutMainLoop();
+
+	return 0;
 }
