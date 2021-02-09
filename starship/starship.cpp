92ccf10e741cc9c6b784f62df5b3946eddad5fd5
kanissh <kanissh11@gmail.com>
2021-02-09 12:04:54 +0530

    Added SOIL2

--- starship/starship.cpp
+++ starship/starship.cpp
@@ -39,6 +39,17 @@ GLfloat bottomFinHeight = 4;
 GLuint tex;
 
 
+GLuint tex_2d = SOIL_load_OGL_texture
+(
+	"img.png",
+	SOIL_LOAD_AUTO,
+	SOIL_CREATE_NEW_ID,
+	SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+);
+
+
+
+
 void init() {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glEnable(GL_DEPTH_TEST);
